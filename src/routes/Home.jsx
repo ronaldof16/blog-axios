52f51efea5c4c445e8a2a7518f86a58d5735936e
ronaldof16 <ronaldof16@hotmail.com>
2024-04-05 16:32:38 -0300

    Exibindo os posts na tela com o método get e fazendo um map na lista dos posts

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-const Home = () => {
+import "./Home.css";
+
+
+const Home = () => { 
+
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async() => {
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      const data = response.data;
+      setPosts(data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
   return (
-    <div>Home</div>
+    <div className="home">
+      <h1>Últimos Posts</h1>
+      {posts.length === 0 ? <p>Carregando...</p> : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+          </div>
+        ))
+      )}
+    </div>
   )
 }
 
