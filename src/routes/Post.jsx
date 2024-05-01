fe1aa062232710832e8d42e0f2f09db0fa802ea7
ronaldof16 <ronaldof16@hotmail.com>
2024-05-01 16:16:04 -0300

    criação da página para carregar um post especícfico

---
+++ src/routes/Post.jsx
@@ -0,0 +1,37 @@
+import blogFetch from "../axios/config";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import "./Post.css"
+
+const Post = () => {
+    const [post, setPost] = useState({});
+    const {id} = useParams();
+
+    const getPost = async () => {
+        try {
+            const response = await blogFetch.get(`/posts/${id}`);
+            const data = response.data;
+            setPost(data)
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    useEffect(() => {
+        getPost();
+    }, []);
+
+  return (
+    <div className="post-container">
+        {!post.title ? <p>Carregando...</p> : (
+            <div className="post">
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+            </div>
+        )}
+    </div>
+  )
+}
+
+export default Post
